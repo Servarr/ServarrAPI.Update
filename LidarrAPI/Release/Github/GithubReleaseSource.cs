Radarr/RadarrAPI.Update

Fix the update server.

RadarrAPI/Release/Github/GithubReleaseSource.cs
@@ -41,12 +41,13 @@ namespace RadarrAPI.Release.Github
                 throw new ArgumentException("ReleaseBranch must not be unknown when fetching releases.");
             }
             
-            var releases = await _gitHubClient.Repository.Release.GetAll("Radarr", "Radarr");
-            var validReleases = releases.Where(r =>
-                r.TagName.StartsWith("v") &&
-                VersionUtil.IsValid(r.TagName.Substring(1)) &&
-                r.Prerelease == (ReleaseBranch == Branch.Develop))
-                .Reverse();
+            var releases = (await _gitHubClient.Repository.Release.GetAll("Radarr", "Radarr")).ToArray();
+            var validReleases = releases
+                .Take(3)
+                .Where(r =>
+                    r.TagName.StartsWith("v") && VersionUtil.IsValid(r.TagName.Substring(1)) &&
+                    r.Prerelease == (ReleaseBranch == Branch.Develop)
+                ).Reverse();
 
             foreach (var release in validReleases)
             {
@@ -70,32 +71,42 @@ namespace RadarrAPI.Release.Github
                         Branch = ReleaseBranch
                     };
 
-                    // Parse changes
-                    var releaseBody = release.Body;
-
-                    var features = RegexUtil.ReleaseFeaturesGroup.Match(releaseBody);
-                    if (features.Success)
-                    {
-                        foreach (Match match in RegexUtil.ReleaseChange.Matches(features.Groups["features"].Value))
-                        {
-                            if (match.Success) updateEntity.New.Add(match.Groups["text"].Value);
-                        }
-                    }
-
-                    var fixes = RegexUtil.ReleaseFixesGroup.Match(releaseBody);
-                    if (fixes.Success)
-                    {
-                        foreach (Match match in RegexUtil.ReleaseChange.Matches(fixes.Groups["fixes"].Value))
-                        {
-                            if (match.Success) updateEntity.Fixed.Add(match.Groups["text"].Value);
-                        }
-                    }
-
                     // Start tracking this object
                     await _database.AddAsync(updateEntity);
                 }
 
-                // Process releases
+                // Parse changes
+                var releaseBody = release.Body;
+
+                var features = RegexUtil.ReleaseFeaturesGroup.Match(releaseBody);
+                if (features.Success)
+                {
+                    updateEntity.New.Clear();
+
+                    foreach (Match match in RegexUtil.ReleaseChange.Matches(features.Groups["features"].Value))
+                    {
+                        if (match.Success)
+                        {
+                            updateEntity.New.Add(match.Groups["text"].Value);
+                        }
+                    }
+                }
+
+                var fixes = RegexUtil.ReleaseFixesGroup.Match(releaseBody);
+                if (fixes.Success)
+                {
+                    updateEntity.Fixed.Clear();
+
+                    foreach (Match match in RegexUtil.ReleaseChange.Matches(fixes.Groups["fixes"].Value))
+                    {
+                        if (match.Success)
+                        {
+                            updateEntity.Fixed.Add(match.Groups["text"].Value);
+                        }
+                    }
+                }
+
+                // Process release files.
                 foreach (var releaseAsset in release.Assets)
                 {
                     // Detect target operating system.

Servarr/ServarrAPI.Update

LidarrAPI/Release/Github/GithubReleaseSource.cs
@@ -39,13 +39,14 @@ namespace LidarrAPI.Release.Github
             {
                 throw new ArgumentException("ReleaseBranch must not be unknown when fetching releases.");
             }
-
-            var releases = await _gitHubClient.Repository.Release.GetAll("Lidarr", "Lidarr");
-            var validReleases = releases.Where(r =>
-                    r.TagName.StartsWith("v") &&
-                    VersionUtil.IsValid(r.TagName.Substring(1)) &&
-                    r.Prerelease == (ReleaseBranch == Branch.Develop))
-                .Reverse();
+            
+            var releases = (await _gitHubClient.Repository.Release.GetAll("lidarr", "Lidarr")).ToArray();
+            var validReleases = releases
+                .Take(3)
+                .Where(r =>
+                    r.TagName.StartsWith("v") && VersionUtil.IsValid(r.TagName.Substring(1)) &&
+                    r.Prerelease == (ReleaseBranch == Branch.Develop)
+                ).Reverse();
 
             foreach (var release in validReleases)
             {
@@ -69,32 +70,43 @@ namespace LidarrAPI.Release.Github
                         Branch = ReleaseBranch
                     };
 
-                    // Parse changes
-                    var releaseBody = release.Body;
-
-                    var features = RegexUtil.ReleaseFeaturesGroup.Match(releaseBody);
-                    if (features.Success)
-                    {
-                        foreach (Match match in RegexUtil.ReleaseChange.Matches(features.Groups["features"].Value))
-                        {
-                            if (match.Success) updateEntity.New.Add(match.Groups["text"].Value);
-                        }
-                    }
-
-                    var fixes = RegexUtil.ReleaseFixesGroup.Match(releaseBody);
-                    if (fixes.Success)
-                    {
-                        foreach (Match match in RegexUtil.ReleaseChange.Matches(fixes.Groups["fixes"].Value))
-                        {
-                            if (match.Success) updateEntity.Fixed.Add(match.Groups["text"].Value);
-                        }
-                    }
-
                     // Start tracking this object
                     await _database.AddAsync(updateEntity);
                 }
 
-                // Process releases
+                // Parse changes
+                var releaseBody = release.Body;
+                
+
+                var features = RegexUtil.ReleaseFeaturesGroup.Match(releaseBody);
+                if (features.Success)
+                {
+                    updateEntity.New.Clear();
+
+                    foreach (Match match in RegexUtil.ReleaseChange.Matches(features.Groups["features"].Value))
+                    {
+                        if (match.Success)
+                        {
+                            updateEntity.New.Add(match.Groups["text"].Value);
+                        }
+                    }
+                }
+
+                var fixes = RegexUtil.ReleaseFixesGroup.Match(releaseBody);
+                if (fixes.Success)
+                {
+                    updateEntity.Fixed.Clear();
+
+                    foreach (Match match in RegexUtil.ReleaseChange.Matches(fixes.Groups["fixes"].Value))
+                    {
+                        if (match.Success)
+                        {
+                            updateEntity.Fixed.Add(match.Groups["text"].Value);
+                        }
+                    }
+                }
+
+                // Process release files.
                 foreach (var releaseAsset in release.Assets)
                 {
                     // Detect target operating system.