7a071759e7d4d3cac0b0935e10d84916056d872d
ta264 <ta264@users.noreply.github.com>
2020-07-18 15:11:32 +0100

    Execute refresh in background hosted servicd

--- src/ServarrAPI/Release/ReleaseService.cs
+++ src/ServarrAPI/Release/ReleaseService.cs
@@ -1,27 +1,14 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using ServarrAPI.Cloudflare;
-using ServarrAPI.Release.Azure;
-using ServarrAPI.Release.Github;
 
 namespace ServarrAPI.Release
 {
     public class ReleaseService
     {
-        private static readonly ConcurrentDictionary<Type, SemaphoreSlim> ReleaseLocks;
-
         private readonly IServiceProvider _serviceProvider;
         private readonly ICloudflareProxy _cloudflare;
-
-        static ReleaseService()
-        {
-            ReleaseLocks = new ConcurrentDictionary<Type, SemaphoreSlim>();
-            ReleaseLocks.TryAdd(typeof(GithubReleaseSource), new SemaphoreSlim(1, 1));
-            ReleaseLocks.TryAdd(typeof(AzureReleaseSource), new SemaphoreSlim(1, 1));
-        }
 
         public ReleaseService(IServiceProvider serviceProvider,
                               ICloudflareProxy cloudflare)
@@ -32,32 +19,10 @@ namespace ServarrAPI.Release
 
         public async Task UpdateReleasesAsync(Type releaseSource)
         {
-            if (!ReleaseLocks.TryGetValue(releaseSource, out var releaseLock))
-            {
-                throw new NotImplementedException($"{releaseSource} does not have a release lock.");
-            }
+            var releaseSourceInstance = (ReleaseSourceBase)_serviceProvider.GetRequiredService(releaseSource);
 
-            var obtainedLock = false;
-
-            try
-            {
-                obtainedLock = await releaseLock.WaitAsync(TimeSpan.FromMinutes(5)).ConfigureAwait(false);
-
-                if (obtainedLock)
-                {
-                    var releaseSourceInstance = (ReleaseSourceBase)_serviceProvider.GetRequiredService(releaseSource);
-
-                    var updatedBranches = await releaseSourceInstance.StartFetchReleasesAsync().ConfigureAwait(false);
-                    await _cloudflare.InvalidateBranches(updatedBranches).ConfigureAwait(false);
-                }
-            }
-            finally
-            {
-                if (obtainedLock)
-                {
-                    releaseLock.Release();
-                }
-            }
+            var updatedBranches = await releaseSourceInstance.StartFetchReleasesAsync().ConfigureAwait(false);
+            await _cloudflare.InvalidateBranches(updatedBranches).ConfigureAwait(false);
         }
     }
 }
