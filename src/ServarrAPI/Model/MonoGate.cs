d19703e34fd85ee79eb05657ac1b9f8e9d75e00a
ta264 <ta264@users.noreply.github.com>
2021-03-18 21:32:41 +0000

    New: Gate by mono version also

---
+++ src/ServarrAPI/Model/MonoGate.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ServarrAPI.Model
+{
+    public class MonoGate
+    {
+        public Version MonoVersion { get; set; }
+        public Version MaxUpgradeVersion { get; set; }
+    }
+}
