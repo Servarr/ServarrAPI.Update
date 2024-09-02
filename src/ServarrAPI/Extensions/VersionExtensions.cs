0918b1206d2c52caf3d207b4828581d45561c73c
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-09-02 08:32:36 +0300

    Fixed: ToIntVersion overflow

--- src/ServarrAPI/Extensions/VersionExtensions.cs
+++ src/ServarrAPI/Extensions/VersionExtensions.cs
@@ -6,7 +6,7 @@ namespace ServarrAPI.Extensions
     {
         public static long ToIntVersion(this Version version)
         {
-            return (version.Major * 10000000000) + (version.Minor * 100000000) + (version.Build * 1000000) + version.Revision;
+            return (version.Major * 10000000000L) + (version.Minor * 100000000L) + (version.Build * 1000000L) + version.Revision;
         }
     }
 }
